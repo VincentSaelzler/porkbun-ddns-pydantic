2c29e68eee63411080ffdc7cc7d15576a7ecaab4
Vincent Saelzler <vincent.saelzler@gmail.com>
2024-05-06 17:23:34 +0000

    log failure output

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from sys import argv
+from datetime import datetime, UTC
 
 
 class Person(BaseModel):
@@ -6,8 +8,28 @@ class Person(BaseModel):
     ln: str
 
 
-p = Person(fn="v", ln="s")
+def main(domain_names: list[str]):
+    # p = Person(fn="v", ln="s")
 
-file_path = "person.json"
-with open(file_path, "w") as file:
-    file.write(p.json())
+    # file_path = "person.json"
+    # with open(file_path, "w") as file:
+    #     file.write(p.json())
+    print("in main")
+
+
+if __name__ == "__main__":
+
+    # log the start time
+    now_str = datetime.now(UTC).isoformat()
+
+    # get incoming args (list of domain names)
+    # skip the first argument (name of the script)
+    domain_names = argv[1:]
+
+    try:
+        main(domain_names)
+    except Exception as e:
+        # create a file to log and debug failures
+        file_path = "DDNS-FAILURE.txt"
+        with open(file_path, "w") as file:
+            file.write(now_str + "\n" + str(e))
